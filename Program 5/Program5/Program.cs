fa995d4277493218d66f30ce4b7fab71b8b55f30
Grant Miller <grantwmiller0@gmail.com>
2019-04-11 18:00:23 -0400

    Add files via upload

---
+++ Program 5/Program5/Program.cs
@@ -0,0 +1,103 @@
+﻿// Grading ID: C6485
+// Course: CIS200-01
+// Program: 5
+// Due: December 5, 2017 11:59 PM
+// Description: Explores the use of binary search trees and generic classes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    // Contains the test data for the TreeNode class
+
+    // class TreeNode declaration
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 8, 2, 4, 3, 1, 7, 5, 6, 9, 10 };
+            double[] doubleArray = { 8.8, 2.2, 4.4, 3.3, 1.1, 7.7, 5.5, 6.6, 9.9, 10.10};
+            string[] stringArray =
+               {"eight", "two", "four","three", "one", "seven", "five", "six", "nine", "ten"};
+
+            // create int Tree
+            Tree<int> intTree = new Tree<int>();
+            PopulateIntTree(intArray, intTree, nameof(intTree));
+            TraverseTree(intTree, nameof(intTree));
+
+            // create double Tree
+            Tree<double> doubleTree = new Tree<double>();
+            PopulateDoubleTree(doubleArray, doubleTree, nameof(doubleTree));
+            TraverseTree(doubleTree, nameof(doubleTree));
+
+            // create string Tree
+            Tree<string> stringTree = new Tree<string>();
+            PopulateStringTree(stringArray, stringTree, nameof(stringTree));
+            TraverseTree(stringTree, nameof(stringTree));
+        }
+
+        // populate Tree with array elements
+        private static void PopulateIntTree(Array array, Tree<int> tree, string name)
+        {
+            Console.WriteLine($"\n\n\nInserting into {name}:");
+
+            foreach (int data in array)
+            {
+                Console.Write($"{data} ");
+                tree.InsertNode(data);
+            }
+        }
+
+        private static void PopulateDoubleTree(Array array, Tree<double> tree, string name)
+        {
+            Console.WriteLine($"\n\n\nInserting into {name}:");
+
+            foreach (double data in array)
+            {
+                Console.Write($"{data} ");
+                tree.InsertNode(data);
+            }
+        }
+
+        private static void PopulateStringTree(Array array, Tree<string> tree, string name)
+        {
+            Console.WriteLine($"\n\n\nInserting into {name}:");
+
+            foreach (string data in array)
+            {
+                Console.Write($"{data} ");
+                tree.InsertNode(data);
+            }
+        }
+
+        // perform traversals
+        private static void TraverseTree(Tree<int> tree, string treeType)
+        {
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {treeType}");
+            tree.InorderTraversal();
+        }
+
+        private static void TraverseDoubleTree(Tree<double> tree, string treeType)
+        {
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {treeType}");
+            tree.InorderTraversal();
+        }
+
+        private static void TraverseStringTree(Tree<string> tree, string treeType)
+        {
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {treeType}");
+            tree.InorderTraversal();
+        }
+
+
+
+    }
+    
+}
---
+++ Program 5/Program5/TreeNode.cs
@@ -0,0 +1,150 @@
+﻿// Grading ID: C6485
+// Course: CIS200-01
+// Program: 5
+// Due: December 5, 2017 11:59 PM
+// Description: Explores the use of binary search trees and generic classes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    // class TreeNode declaration
+    class TreeNode<T> where T : IComparable<T>
+    {
+        // automatic property LeftNode
+        public TreeNode<T> LeftNode { get; set; }
+
+        // automatic property Data
+        public T Data { get; private set; }
+
+        // automatic property RightNode
+        public TreeNode<T> RightNode { get; set; }
+
+        // initialize Data and make this a leaf node
+        public TreeNode(T nodeData)
+        {
+            Data = nodeData;
+        }
+
+        // insert TreeNode into Tree that contains nodes;
+        // ignore duplicate values
+        public void Insert(T insertValue)
+        {
+            if (insertValue.CompareTo(Data) < 0) // insert in left subtree
+            {
+                // insert new TreeNode
+                if (LeftNode == null)
+                {
+                    LeftNode = new TreeNode<T>(insertValue);
+                }
+                else // continue traversing left subtree
+                {
+                    LeftNode.Insert(insertValue);
+                }
+            }
+            else if (insertValue.CompareTo(Data) > 0) // insert in right
+            {
+                // insert new TreeNode
+                if (RightNode == null)
+                {
+                    RightNode = new TreeNode<T>(insertValue);
+                }
+                else // continue traversing right subtree
+                {
+                    RightNode.Insert(insertValue);
+                }
+            }
+        }
+    }
+
+    // class Tree declaration
+    public class Tree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> root;
+
+        // Insert a new node in the binary search tree.
+        // If the root node is null, create the root node here.
+        // Otherwise, call the insert method of class TreeNode.
+        public void InsertNode(T insertValue)
+        {
+            if (root == null)
+            {
+                root = new TreeNode<T>(insertValue);
+            }
+            else
+            {
+                root.Insert(insertValue);
+            }
+        }
+
+        // begin preorder traversal
+        public void PreorderTraversal()
+        {
+            PreorderHelper(root);
+        }
+
+        // recursive method to perform preorder traversal
+        private void PreorderHelper(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                // output node Data
+                Console.Write($"{node.Data} ");
+
+                // traverse left subtree
+                PreorderHelper(node.LeftNode);
+
+                // traverse right subtree
+                PreorderHelper(node.RightNode);
+            }
+        }
+
+        // begin inorder traversal
+        public void InorderTraversal()
+        {
+            InorderHelper(root);
+        }
+
+        // recursive method to perform inorder traversal
+        private void InorderHelper(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                // traverse left subtree
+                InorderHelper(node.LeftNode);
+
+                // output node data
+                Console.Write($"{node.Data} ");
+
+                // traverse right subtree
+                InorderHelper(node.RightNode);
+            }
+        }
+
+        // begin postorder traversal
+        public void PostorderTraversal()
+        {
+            PostorderHelper(root);
+        }
+
+        // recursive method to perform postorder traversal
+        private void PostorderHelper(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                // traverse left subtree
+                PostorderHelper(node.LeftNode);
+
+                // traverse right subtree
+                PostorderHelper(node.RightNode);
+
+                // output node Data
+                Console.Write($"{node.Data} ");
+            }
+        }
+    }
+}
